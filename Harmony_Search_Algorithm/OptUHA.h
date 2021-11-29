fdefb5191983aa261dff58bebe9bc50513d1b963
Meriem <tamrabet99m@outlook.fr>
2021-11-29 17:29:21 +0100

    Ajout de fichier fonction et modification

--- Harmony_Search_Algorithm/OptUHA.h
+++ Harmony_Search_Algorithm/OptUHA.h
@@ -9,11 +9,13 @@ using namespace std;
 typedef double indType;
 typedef indType* solution;
 
+
+
 class OptUHA
 {
 protected:
-	size_t								pop_size = 10; // taille de la population
-	size_t								dimension = 2; // dimension du problème
+	size_t								pop_size = 30; // taille de la population
+	size_t								dimension = 30 ; // dimension du problème
 	size_t								total_func_evals = 2500; // nombre total d'évaluation
 	size_t								max_func_eval = total_func_evals * pop_size; // nombre total d'appel de la fonction objectif
 	bool								optimization_mode = 0; // 0 = min; 1 = max;
@@ -32,7 +34,7 @@ protected :
 	vector<solution> GenerateRandomPop();
 	double generate_random_double();
 	int generate_random_int(int from, int to);
-	solution create_new_individual();
+	solution create_new_solution();
 	void print_solution(double* indv, double fitness);
 	solution GenerateNewSolution(int current_ind_idx);
 	void check_bound_pop(vector<solution>& population);
--- Harmony_Search_Algorithm/myAlgorithm.h
+++ Harmony_Search_Algorithm/myAlgorithm.h
@@ -4,25 +4,26 @@
 #include "OptUHA.h"
 #include "objective_func.h"
 
-class myAlgorithm : OptUHA
+class harmonySearch : OptUHA
 {
 protected:
 	 // déclarer les paramètres de votre algorithme ici
-	 
+
 	vector<solution>					population;
 
 
-protected: 
+protected:
 	void UpdatePoulation(int func_num, int idx, solution new_sol);
-	indvidual FindBestSolution(int func_num, double &fitness);
+	solution FindBestSolution(int func_num, double &fitness);
 	void evaluate_pop(int func_num);
 	double evaluate_individual(int func_num, solution ind);
 	void update_fitness(int idx, double fitness_val);
 public:
 	double solve(int func_num);
-	myAlgorithm( );  // mettre un constructeur avec des paramètres ici
-	~myAlgorithm();
-	
+	harmonySearch( );  // mettre un constructeur avec des paramètres ici
+	~harmonySearch();
+
+
 	// Ajouter d'autres fonctions liées à votre algorithme ici
 };
 
