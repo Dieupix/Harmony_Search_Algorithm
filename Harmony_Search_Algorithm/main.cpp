16f20b6697c9ce65f98432cf0793750e8a1d330a
Manu <manuel.barrere@uha.fr>
2021-11-29 16:16:41 +0100

    Creation projet

--- Harmony_Search_Algorithm/main.cpp
+++ Harmony_Search_Algorithm/main.cpp
@@ -1,9 +1,29 @@
-#include <iostream>
+// OptUHA.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
 
-using namespace std;
+#include <iostream>
+#include <vector>
+#include "myAlgorithm.h"
+#include "OptUHA.h"
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    srand((unsigned)time(NULL));
+    int num_runs = 10;
+    int num_funcs = 12; 
+    vector<vector<double>> obj_vals_array(num_funcs);
+    for (int func = 0; func < num_funcs; func++)
+    {
+        for (int run = 0; run < num_runs; run++)
+        {
+            myAlgorithm* myAlgo = new myAlgorithm();
+            double fitness = myAlgo->solve(func + 1);
+            obj_vals_array[func].push_back(fitness);
+            cout << "function: " << func << " obj_val: " << fitness << endl;
+            if (myAlgo)
+                delete myAlgo; myAlgo = NULL;
+        }
+    }
+
 }
+
